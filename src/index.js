5b4f314a2be3ea13b844310f4dc5d29a9d69bb0a
yusufnviiri <yusufnviiri@gmail.com>
2022-04-29 10:14:09 +0300

    Modify index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './style.css';
-import '@fortawesome/fontawesome-free/js/fontawesome.js';
-import '@fortawesome/fontawesome-free/js/solid.js';
-import '@fortawesome/fontawesome-free/js/regular.js';
-import '@fortawesome/fontawesome-free/js/brands.js';
 
 const listContent = document.querySelector('.list-content');
 
